524522508bc4d2ce03aad62f2afbdf44d62fdfc5
Matheus Leal Gomes da Silva <matheusleal@tre-pa.jus.br>
2023-12-27 16:54:59 -0300

    feat: ajustes no designer da navegação

--- src/app/@core/layout/side-nav-outer-toolbar/side-nav-outer-toolbar.component.ts
+++ src/app/@core/layout/side-nav-outer-toolbar/side-nav-outer-toolbar.component.ts
@@ -3,17 +3,22 @@ import {CommonModule} from '@angular/common';
 import {ClrDropdownModule, ClrVerticalNavModule} from '@clr/angular';
 import {RouterLink, RouterLinkActive} from '@angular/router';
 import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
-import {HeaderComponent} from "../components/header/header.component";
-import {SideNavigationMenuComponent} from "../components/side-navigation-menu/side-navigation-menu.component";
+import {APP_NAVIGATION} from "../../../app-navigation";
 
 @Component({
   selector: 'app-side-nav-outer-toolbar',
   standalone: true,
-  imports: [CommonModule, ClrVerticalNavModule, RouterLink, RouterLinkActive, BrowserAnimationsModule, ClrDropdownModule, HeaderComponent, SideNavigationMenuComponent],
+  imports: [CommonModule, ClrVerticalNavModule, RouterLink, RouterLinkActive, BrowserAnimationsModule, ClrDropdownModule],
   templateUrl: './side-nav-outer-toolbar.component.html',
   styleUrls: ['./side-nav-outer-toolbar.component.scss']
 })
 export class SideNavOuterToolbarComponent {
+  protected title: string = 'app-side-nav-outer-bar';
+  protected appNavigation = APP_NAVIGATION;
+  dropdownItems = [
+    {text: 'Sobre', icon: 'info-circle'},
+    {text: 'Logout', icon: 'logout'},
+  ];
 
 
 }
