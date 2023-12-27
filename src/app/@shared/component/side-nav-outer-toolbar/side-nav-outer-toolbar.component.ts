c6fb3d3e54c89cb3d585da6222dc60fbdcc89fec
Matheus Leal <matheuslealpa@gmaill.com>
2023-12-27 00:10:41 -0300

    refactor: side navouter toolbar

--- src/app/@shared/component/side-nav-outer-toolbar/side-nav-outer-toolbar.component.ts
+++ src/app/@shared/component/side-nav-outer-toolbar/side-nav-outer-toolbar.component.ts
@@ -1,10 +1,14 @@
 import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ClrIconCustomTag, ClrIconModule, ClrVerticalNavModule } from '@clr/angular';
+import { APP_NAVIGATION } from 'src/app/app-navigation';
+import { RouterLink } from '@angular/router';
+import { CdsIcon } from '@cds/core/icon/icon.element';
 
 @Component({
   selector: 'app-side-nav-outer-toolbar',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ClrVerticalNavModule, RouterLink, ClrIconModule],
   templateUrl: './side-nav-outer-toolbar.component.html',
   styleUrls: ['./side-nav-outer-toolbar.component.scss']
 })
@@ -12,5 +16,8 @@ export class SideNavOuterToolbarComponent {
 
   @Input()
   title?: string
+  // collapsed?: boolean = false
+
+  appNavigation = APP_NAVIGATION;
 
 }
---
+++ src/app/app-navigation.ts
@@ -0,0 +1,21 @@
+export const APP_NAVIGATION = [
+    {
+        caption: 'Home',
+        path: '/',
+        icon: 'home'
+    },
+    {
+        caption: 'Configurações',
+        icon: 'administrator',
+        items: [
+            {
+                caption: 'Configuração 1',
+                path: '/conf1'
+            },
+            {
+                caption: 'Configuração 2',
+                path: '/conf2'
+            }
+        ]
+    }
+]
